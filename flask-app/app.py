9ab61ef4c83d676833c8d81e66417682f7c66be4
nordaxion <jedmagracia@gmail.com>
2020-03-07 16:56:17 -0500

    updated database and framework

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, request, session, redirect, flash, url_for
-from .db import *
+from cs50 import SQL
 from tempfile import mkdtemp
 from flask_session import Session
 from functools import wraps
+import datetime
 from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
@@ -13,6 +14,8 @@ app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+
+db = SQL("sqlite:///calorievision.db")
 
 
 def login_required(f):
@@ -37,17 +40,14 @@ def aboutus():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    # TODO
     session.clear()
 
     if request.method == "POST":
 
         if not request.form.get("username"):
-            # TODO
             return render_template("error.html")
 
         elif not request.form.get("password"):
-            # TODO
             return render_template("error.html")
 
         rows = db.execute("SELECT * FROM users WHERE username = :username",
@@ -56,7 +56,7 @@ def login():
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
             return render_template("error.html")
 
-        session["user_id"] = rows[0]["id"]
+        session["user_id"] = rows[0]["user_id"]
 
         return redirect(url_for("homepage"))
 
@@ -64,19 +64,18 @@ def login():
         return render_template("login.html")
 
 
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    # TODO
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
     if request.method == "POST":
 
         if not request.form.get("username"):
-            return render_template("error.html")
+            return render_template("login.html")
 
         elif not request.form.get("password"):
-            return render_template("error.html")
+            return render_template("login.html")
 
         elif not request.form.get("password") == request.form.get("confirmation"):
-            return render_template("error.html")
+            return render_template("login.html")
 
         hashed_pw = generate_password_hash(request.form.get("password"))
         new_user_id = db.execute("INSERT INTO users (username, hash) VALUES(:username, :hash)",
@@ -84,7 +83,7 @@ def register():
                                  hash=hashed_pw)
 
         if not new_user_id:
-            return render_template("error.html")
+            return render_template("login.html")
 
         session["user_id"] = new_user_id
 
@@ -93,14 +92,14 @@ def register():
         return redirect(url_for("homepage"))
 
     else:
-        return render_template("register.html")
+        return render_template("signup.html")
 
 
 @app.route("/homepage", methods=["GET", "POST"])
 @login_required
 def homepage():
-    # TODO
-    rows = db.execute("SELECT date_created, food_name, calories FROM (SELECT * FROM transactions WHERE date_created "
+
+    rows = db.execute("SELECT date_created, name, calories FROM (SELECT * FROM transactions WHERE date_created "
                       "BETWEEN date('now', '-7 day') and date('now')) as ts, users, foods WHERE users.user_id = "
                       "ts.user_id AND foods.food_id = ts.food_id")
     return render_template("homepage.html", rows=rows)
@@ -116,13 +115,12 @@ def scan():
 @app.route("/logout")
 @login_required
 def logout():
-    # TODO
     session.clear()
-    return redirect(url_for("aboutUs"))
+    return redirect(url_for("aboutus"))
 
 
 @app.route("/profile")
 @login_required
 def profile():
     # TODO
-    return render_template("profile.html")
+    return render_template("profile.html", name=name, username=username, member_since=member_since)
--- flask-app/db.py
+++ flask-app/db.py
@@ -1,4 +1,15 @@
-import sqlite3
+from cs50 import SQL
+import csv
 
-conn = sqlite3.connect("calorievision.db")
-db = conn.cursor()
+db = SQL("sqlite:///calorievision.db")
+
+with open("nutrition_scrappe.csv", "r") as food_data:
+    csv_reader = csv.reader(food_data)
+
+    next(csv_reader)
+
+    for row in csv_reader:
+        print(row)
+        db.execute("INSERT INTO foods(name, restaurant, min_val, max_val, mean_val) VALUES(:name, :restaurant, "
+                   ":min_val, :max_val, :mean_val",
+                   name=str(row[1]), restaurant=row[2], min_val=row[3], max_val=row[4], mean_val=row[5])
