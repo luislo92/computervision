2ce8cb73f2d39eac1f1f48dbc352fd545e8749c7
nordaxion <45746064+nordaxion@users.noreply.github.com>
2020-03-06 22:24:25 -0500

    uploading updated flask

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,10 +1,35 @@
 from flask import Flask, render_template, request
 from cs50 import SQL
-from flask_session import 
+from flask_session import Session
+
 
 app = Flask(__name__)
 
 
 @app.route("/")
+def aboutUs():
+    return render_template("aboutUs.html")
+
+
+@app.route("/login")
+def login():
+    # TODO
+    pass
+
+
+@app.route("/signUp")
+def signUp():
+    # TODO
+    pass
+
+
+@app.route("/homepage")
 def homepage():
-    return render_template("homepage.html")
+    # TODO
+    pass
+
+
+@app.route("/scan")
+def scan():
+    # TODO
+    pass
