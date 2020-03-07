5d091ac9bcad6d0ea9ded6bcb220c266b7df0088
nordaxion <45746064+nordaxion@users.noreply.github.com>
2020-03-07 09:38:47 -0500

    updated Flask

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask, render_template, request, session, redirect, flash, url_for
 from cs50 import SQL
 from tempfile import mkdtemp
 from flask_session import Session
 from functools import wraps
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 
@@ -32,29 +33,80 @@ def login_required(f):
     return decorated_function
 
 
-@app.route("/")
+@app.route("/aboutUs")
 def aboutUs():
     return render_template("aboutUs.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
-@login_required
 def login():
     # TODO
-    pass
+    session.clear()
+
+    if request.method == "POST":
+
+        if not request.form.get("username"):
+            # TODO
+            return render_template("error.html")
+
+        elif not request.form.get("password"):
+            # TODO
+            return render_template("error.html")
+
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                          username=request.form.get("username"))
+
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return render_template("error.html")
+
+        session["user_id"] = rows[0]["id"]
+
+        return redirect(url_for("homepage"))
+
+    else:
+        return render_template("login.html")
 
 
-@app.route("/signUp", methods=["GET", "POST"])
-def signUp():
+@app.route("/register", methods=["GET", "POST"])
+def register():
     # TODO
-    pass
+    if request.method == "POST":
+
+        if not request.form.get("username"):
+            return render_template("error.html")
+
+        elif not request.form.get("password"):
+            return render_template("error.html")
+
+        elif not request.form.get("password") == request.form.get("confirmation"):
+            return render_template("error.html")
+
+        hashed_pw = generate_password_hash(request.form.get("password"))
+        new_user_id = db.execute("INSERT INTO users (username, hash) VALUES(:username, :hash)",
+                                 username=request.form.get("username"),
+                                 hash=hashed_pw)
+
+        if not new_user_id:
+            return render_template("error.html")
+
+        session["user_id"] = new_user_id
+
+        flash("You have been registered")
+
+        return redirect(url_for("homepage"))
+
+    else:
+        return render_template("register.html")
 
 
 @app.route("/homepage", methods=["GET", "POST"])
 @login_required
 def homepage():
     # TODO
-    pass
+    rows = db.execute("SELECT date_created, food_name, calories FROM (SELECT * FROM transactions WHERE date_created "
+                      "BETWEEN date('now', '-7 day') and date('now')) as ts, users, foods WHERE users.user_id = "
+                      "ts.user_id AND foods.food_id = ts.food_id")
+    return render_template("homepage.html", rows=rows)
 
 
 @app.route("/scan", methods=["GET", "POST"])
@@ -68,4 +120,12 @@ def scan():
 @login_required
 def logout():
     # TODO
-    pass
+    session.clear()
+    return redirect(url_for("aboutUs"))
+
+
+@app.route("/profile")
+@login_required
+def profile():
+    # TODO
+    return render_template("profile.html")
