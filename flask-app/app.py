0d96ce6f71073683cb21365050e37359335fc54a
luislo92 <luislosada92@gmail.com>
2020-03-08 01:56:03 -0500

    final version of the google cloud vision algo + bert

--- flask-app/app.py
+++ flask-app/app.py
@@ -4,6 +4,7 @@ from tempfile import mkdtemp
 from flask_session import Session
 from functools import wraps
 from werkzeug.security import check_password_hash, generate_password_hash
+import vision
 
 app = Flask(__name__)
 
@@ -12,8 +13,6 @@ app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-
-db = SQL("sqlite:///calorievision.db")
 
 
 def login_required(f):
